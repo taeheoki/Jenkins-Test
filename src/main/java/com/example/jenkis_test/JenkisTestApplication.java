9b636f8ff13df50640b9b9e65bb34500e03914cd
taeheoki <dkqjwl2365@naver.com>
2023-12-04 13:30:29 +0900

    🧑🏻‍💻feat: first commit

---
+++ src/main/java/com/example/jenkis_test/JenkisTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkis_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkisTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkis_test/JenkisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkis_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
